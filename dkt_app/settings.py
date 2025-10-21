3566032d0765ea740563edd982c1b8fad5e225f5
ahmerdev <shahwaizsheikh183@gmail.com>
2025-10-21 18:58:09 +0500

    Update ALLOWED_HOSTS for development and production

--- dkt_app/settings.py
+++ dkt_app/settings.py
@@ -32,9 +32,7 @@ SECRET_KEY = 'django-insecure-0oomw^myzexxz(ek51%c-^)g+35*-)*egs4x=&+1ho24%06-jh
 DEBUG = True
 
 # ALLOWED_HOSTS = ['13.51.177.129', 'localhost', '127.0.0.1']
-ALLOWED_HOSTS = [
-    '13.51.177.129',  # aapka public IP
-    'ec2-13-51-177-129.eu-north-1.compute.amazonaws.com']
+ALLOWED_HOSTS = ['13.51.177.129', '127.0.0.1', '13.51.177.129:8000',  '127.0.0.1:8000']
 
 
 
