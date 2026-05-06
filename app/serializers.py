b2215f2268c8efe97241d2ea9d937dcba0a4b8a3
Mohammad Shahwaiz Ahmer <shahwaizsheikh183@gmail.com>
2026-05-06 05:22:25 -0700

    Refactor ProductVariantSerializer to improve methods

--- app/serializers.py
+++ app/serializers.py
@@ -75,14 +75,21 @@ class HeroSerializer(serializers.ModelSerializer):
 
 
 class ProductVariantSerializer(serializers.ModelSerializer):
+        sale_price = serializers.SerializerMethodField()
+        attributes = serializers.SerializerMethodField()
 
-    sale_price = serializers.SerializerMethodField()
+        def get_sale_price(self, obj):
+            attrs = obj.attributes or {}
+            return attrs.get('sale_price', None)
 
-    def get_sale_price(self, obj):
-        return obj.attributes.get('sale_price', None)
-    class Meta:
-        model = ProductVariant
-        fields = ["id", "sku", "price", "sale_price", "stock", "attributes", "is_active", "image"]
+        def get_attributes(self, obj):
+            attrs = dict(obj.attributes or {})
+            attrs.pop('sale_price', None)
+            return attrs
+
+        class Meta:
+            model = ProductVariant
+            fields = ["id", "sku", "price", "sale_price", "stock", "attributes", "is_active", "image"]
 
 
 
